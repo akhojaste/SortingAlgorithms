2896882c31affd7d88bbdd587f482bd3acec8466
Amir Khojaste <amir.khojaste@sunnybrook.ca>
2015-07-16 14:38:57 -0400

    Bubble Sort Method

---
+++ BubbleSort.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+
+
+void printNum(const std::vector<int> num){
+
+	for (int i = 0; i < num.size() - 1; ++i){
+
+		std::cout << num[i] << " ,";
+	}
+
+	std::cout << num[num.size() - 1] << std::endl;
+}
+
+void BubbleSort(std::vector<int> &num){
+
+	unsigned int length = num.size();
+	int temp=0;
+
+	for (int i = 0; i < length-1; ++i){
+
+		for (int j = 0; j < length - 1; ++j){
+
+			if (num[j] <= num[j + 1]){
+				temp = num[j];
+				num[j] = num[j + 1];
+				num[j + 1] = temp;
+			}
+
+
+		}
+
+		printNum(num);
+	}
+
+
+}
+
+
+int main(int argc, char* argv[]){
+  
+  std::vector<int> nums = {6,5,-1,3,-8,4,7,9,2};
+ 	BubbleSort(nums);
+ 	
+ 	return 0;
+
+
+}
